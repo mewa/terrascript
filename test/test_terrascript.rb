fabf2fa6ba6a8c03dee31f38f2e8cf3de68461e7
mewa <marcin.k.chmiel@gmail.com>
2018-04-10 16:33:08 +0200

    Add whitespaces test

--- test/test_terrascript.rb
+++ test/test_terrascript.rb
@@ -33,6 +33,23 @@ EOF
 
 TC_ARG_OUT = TC_BASIC1_OUT
 
+TC_WHITESPACE_IN = <<EOF
+ abc
+	  @inline
+   puts block
+ return
+x blk x
+	tab blk
+	@end
+EOF
+
+TC_WHITESPACE_OUT = <<EOF
+ abc
+x blk x
+	tab blk
+EOF
+
+
 class TestTransform < Minitest::Test
 
   def test(input, output)
@@ -48,5 +65,9 @@ class TestTransform < Minitest::Test
 
   def test_arg
     test(TC_ARG_IN, TC_ARG_OUT)
+  end
+
+  def test_whitespaces
+    test(TC_WHITESPACE_IN, TC_WHITESPACE_OUT)
   end
 end
