7d73b645b4ada00a5712fad6ad517ab2bd9c17d6
mewa <marcin.k.chmiel@gmail.com>
2018-04-10 04:24:07 +0200

    Added basic Transform tests

---
+++ test/test_terrascript.rb
@@ -0,0 +1,52 @@
+require 'terrascript'
+require 'minitest/autorun'
+require 'stringio'
+
+TC_BASIC1_IN = <<EOF
+abc
+@inline
+puts block
+puts "test"
+puts block
+return
+efg
+@end
+EOF
+
+TC_BASIC1_OUT = <<EOF
+abc
+efg
+test
+efg
+EOF
+
+TC_ARG_IN = <<EOF
+abc
+@inline xyz
+puts xyz
+puts "test"
+puts xyz
+return
+efg
+@end
+EOF
+
+TC_ARG_OUT = TC_BASIC1_OUT
+
+class TestTransform < Minitest::Test
+
+  def test(input, output)
+    f = StringIO.new(input)
+    out = StringIO.new
+    Transform.new(f, out).process
+    assert_equal output, out.string
+  end
+
+  def test_basic
+    test(TC_BASIC1_IN, TC_BASIC1_OUT)
+  end
+
+  def test_arg
+    test(TC_ARG_IN, TC_ARG_OUT)
+  end
+end
